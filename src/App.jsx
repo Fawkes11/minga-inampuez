cf686b9dee4a99b924f4cfa6a62fb01a3ed96e5b
Hessler Inampuez <hesslerinampuez@gmail.com>
2023-06-03 18:06:05 -0500

    edit files

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import { AiOutlineMenu } from "react-icons/ai";
+import Logo from "./assets/logo-mangas.png"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="bg-[url('./assets/background-home.png')] h-screen bg-cover bg-no-repeat">
+      {/* SECCION NAVBAR */}
+      <nav className='w-full flex justify-between items-center px-[14px] lg:px-[46px] pt-3 lg:pt-[31px]'>
+        <a href='#'><AiOutlineMenu className='text-3xl md:text-5xl text-violet-900' /></a>
+        <img src={Logo} alt="Logo Mangas" />
+      </nav>
+      {/* SECCION MAIN */}
+      <main className='w-full text-white text-center mt-64 px-[21px] '>
+        <h1 className='text-4xl lg:text-5xl font-bold'>Your favorite comic book store</h1>
+        <p className='md:hidden text-center mt-6'>From classics to novelties, we have everything you need immerse yourself in your favorite universes. Expore our catalog and livve the adventure of your life.</p>
+        <p className='hidden md:block text-2xl mt-4'>Explore our catalog to live the adventure of your life</p>
+        <button className='bg-violet-900 w-full max-w-[240px] h-[68px] rounded-full md:rounded-md text-2xl font-bold lg:font-medium mt-6'>Started</button>
+      </main>
+    </div>
   )
 }
 
